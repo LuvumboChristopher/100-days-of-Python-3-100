0700c183988620796a9a70fe3c24572460947040
Christopher Luvumbo <christopherluvumbo.dev@gmail.com>
2023-11-17 15:39:10 +0100

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+cooking = input("Name a method of cooking: ")
+description = input("What word describer burned food?: ")
+item = input("Name a DIY item: ")
+
+print("MENU")
+print("Saute",food,"with", description, plant, "on a bed of",item)
